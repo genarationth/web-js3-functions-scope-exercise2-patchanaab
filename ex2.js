63abb0bf94f9f46cc807fa9f3d49d535bf21c4e1
patchanaab <165882679+patchanaab@users.noreply.github.com>
2024-05-14 15:27:12 +0700

    exercise 2

---
+++ ex2.js
@@ -0,0 +1,11 @@
+// 2.1 
+function numberEven(number) {
+    return number % 2 === 0;
+}
+console.log(numberEven(8)); 
+console.log(numberEven(9)); 
+
+// 2.2
+const isEven = number => number % 2 === 0;
+console.log(isEven(10)); 
+console.log(isEven(11)); 
